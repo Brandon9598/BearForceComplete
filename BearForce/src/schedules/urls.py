ce16114aa060be8c408f1719e250489378d9a33c
Brandon Hancock <Brandon9598@gmail.com>
2016-07-15 14:33:46 -0400

    Created a new reposity because I messed up

--- BearForce/src/schedules/urls.py
+++ BearForce/src/schedules/urls.py
@@ -2,10 +2,12 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = "schedules"
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
-	url(r'^shift_pick_up/$', views.shift_pick_up, name="Shift pick up"),
-	url(r'^shift_release/$', views.shift_release, name="Shift release"),
-	url(r'^staff/$', views.staff, name="Staff"),
+	url(r'^new_message/$', views.new_message, name="new_message"),
+	url(r'^shift_pick_up/$', views.shift_pick_up, name="shift pick up"),
+	url(r'^shift_release/$', views.shift_release, name="shift release"),
+	url(r'^staff/$', views.staff, name="staff"),
 	url(r'^account/(?P<user_id>[0-9]+)/$', views.account, name="account"),
 ]
--- BearForce/src/schedules/views.py
+++ BearForce/src/schedules/views.py
@@ -8,13 +8,18 @@ from .models import User, News_Messages, Shift
 def index(request):
 	bearforce_worker = User.objects.get(pk=1)
 	shifts_to_work = bearforce_worker.shift_set.all()
+	bearforce_messages = News_Messages.objects.all()
 	context= {
 		"shifts_to_work": shifts_to_work,
 		"bearforce_worker": bearforce_worker,
+		"bearforce_messages": bearforce_messages,
 	}
 	#get_object_or_404
 	return render(request, 'schedules/index.html', context)
 
+def new_message(request):
+	return HttpResponse("Hello, new message view is working.")
+	
 def shift_pick_up(request):
 	return HttpResponse("Hello, shift pickup view is working.")
 
